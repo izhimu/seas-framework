498dbf0d71885cfe8f27cb14ef57005f328f2b04
haoran <conghaoran1226@qq.com>
2023-06-19 16:49:17 +0800

    fix: INSERT_UPDATE问题

--- seas-framework-component/seas-framework-data/src/main/java/com/izhimu/seas/data/entity/BaseEntity.java
+++ seas-framework-component/seas-framework-data/src/main/java/com/izhimu/seas/data/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.Version;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.izhimu.seas.core.annotation.ViewIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -23,32 +24,38 @@ public class BaseEntity extends IdEntity {
     /**
      * 创建人
      */
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @TableField(fill = FieldFill.INSERT)
     private Long createdBy;
     /**
      * 创建时间
      */
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createdTime;
     /**
      * 更新人
      */
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long updatedBy;
     /**
      * 更新时间
      */
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updatedTime;
     /**
      * 版本
      */
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @ViewIgnore
     @Version
     private Integer version;
     /**
      * 逻辑删除
      */
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @ViewIgnore
     @TableLogic
     private Integer logicDel;
