e6b53af129c237bdd620193fef25b4d7f2823688
haorao <haoran@izhimu.cn>
2024-08-03 18:00:08 +0800

    feat: 新增AiChatController

---
+++ seas-framework-server/seas-framework-ai/src/main/java/com/izhimu/seas/ai/controller/AiChatController.java
@@ -0,0 +1,45 @@
+package com.izhimu.seas.ai.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * AI 语言模型控制层
+ *
+ * @author haoran
+ * @version v1.0
+ */
+@RestController
+@RequestMapping("/ai/chat")
+public class AiChatController {
+
+    @Resource
+    private OllamaChatModel chatModel;
+
+    @GetMapping("/generate")
+    public String generate(String input) {
+        UserMessage userMessage = new UserMessage(input);
+        ChatResponse response = chatModel.call(new Prompt(List.of(userMessage)));
+        Generation result = response.getResult();
+        AssistantMessage output = result.getOutput();
+        return output.getContent();
+    }
+
+    @GetMapping("/stream/generate")
+    public Flux<String> generateStream(String input) {
+        UserMessage userMessage = new UserMessage(input);
+        Flux<ChatResponse> response = chatModel.stream(new Prompt(List.of(userMessage)));
+        return response.map(v -> v.getResult().getOutput().getContent());
+    }
+}
