78a0d73cf9838c08df8f1f62f0e060893240f483
haoran <haoran@izhimu.cn>
2024-08-04 14:17:28 +0800

    feat: 测试pgvector

--- seas-framework-server/seas-framework-ai/src/main/java/com/izhimu/seas/ai/controller/AiChatController.java
+++ seas-framework-server/seas-framework-ai/src/main/java/com/izhimu/seas/ai/controller/AiChatController.java
@@ -9,14 +9,18 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
+import java.util.Map;
 
 import static com.izhimu.seas.ai.function.ToolsFunction.FUNCTION_SYSTEM_MANAGER;
 
@@ -32,6 +36,9 @@ public class AiChatController {
 
     @Resource
     private OllamaChatModel chatModel;
+
+    @Resource
+    private VectorStore vectorStore;
 
     @GetMapping("/generate")
     public String generate(String input) {
@@ -50,5 +57,17 @@ public class AiChatController {
     public Flux<String> generateStream(String input) {
         Flux<ChatResponse> response = chatModel.stream(new Prompt(input));
         return response.map(v -> v.getResult().getOutput().getContent());
+    }
+
+    @GetMapping("/documents")
+    public List<Document> getDocuments() {
+        List<Document> documents = List.of(
+                new Document("Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!", Map.of("meta1", "meta1")),
+                new Document("The World is Big and Salvation Lurks Around the Corner"),
+                new Document("You walk forward facing the past and you turn back toward the future.", Map.of("meta2", "meta2")));
+        vectorStore.add(documents);
+        List<Document> documentList = vectorStore.similaritySearch(SearchRequest.query("Spring").withTopK(5));
+        vectorStore.delete(documentList.stream().map(Document::getId).toList());
+        return documentList;
     }
 }
